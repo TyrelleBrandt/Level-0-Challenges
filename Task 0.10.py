aa2a6a190a03a9094a735b3df2378b2f4a5a3f62
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2021-10-21 23:50:25 +0200

    straight line changes

--- Task 0.10.py
+++ Task 0.10.py
@@ -5,9 +5,9 @@ result = []
 if (len(str1) < len(str2)):
     for a in str1:
         if(a in str2):
-            result.append(a)
+            result.append((a)+ ',')
 else:
     for a in str2:
         if(a in str1):
-            result.append(a)
+            result.append((a)+ ', ')
 print('Common letters:', *result)

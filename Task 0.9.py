0150da46ecd51d0a8191b378e1c301e0ebc1fb45
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2021-10-22 00:00:26 +0200

    vowels next to each other

--- Task 0.9.py
+++ Task 0.9.py
@@ -1,10 +1,11 @@
-result = []
-if (len(str1) < len(str2)):
-    for a in str1:
-        if(a in str2):
-            result.append((a)+ ',')
-else:
-    for a in str2:
-        if(a in str1):
-            result.append((a)+ ', ')
-print('Common letters:', *result)
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+word = str.lower("Umuzi")
+
+list1 = list(word)
+list2 = []
+for x in list1:
+    if x.lower() in vowels:
+        list2.append(x)
+vowes = ', '.join(list2)
+print("Vowels: "+vowes)

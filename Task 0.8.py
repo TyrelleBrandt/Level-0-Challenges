93feaa6d237a7a3dde83faedf88713fe0ecae652
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2021-10-21 23:35:52 +0200

    singular and plural added

--- Task 0.8.py
+++ Task 0.8.py
@@ -1,6 +1,14 @@
-number = 550
+number = 121
+a = str(1)
 
 hours = str(number // 60)
 minutes = str(number % 60)
 
-print(hours, "hours"  +  minutes, "minutes")
+if hours == a and minutes == a:
+    print(hours, "hour" + ',' + minutes, "minute")
+elif hours > a and minutes > a:
+    print(hours, "hours" + ',' + minutes, "minutes")
+elif hours == a and minutes > a:
+    print(hours, "hour" + ',' + minutes, "minutes")
+elif hours > a and minutes == a:
+    print(hours, "hours" + ',' + minutes, "minute")

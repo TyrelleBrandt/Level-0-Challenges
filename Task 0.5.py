e8e5c88b036982a6a2467c92a6ff290c6190cb28
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2021-10-21 08:52:27 +0200

    Removed input function

--- Task 0.5.py
+++ Task 0.5.py
@@ -1,5 +1,5 @@
-height = float(input("Height: "))
-base = float(input("Base: "))
+height = 5
+base = 12
 def area_of_triangle(height):
     return (height * base) / 2 
 print(area_of_triangle(height))

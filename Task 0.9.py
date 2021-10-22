137ff5469f2981640a620f452e110f973846320c
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2021-10-22 18:53:37 +0200

    changed function

--- Task 0.9.py
+++ Task 0.9.py
@@ -1,13 +1,15 @@
-vowels = ['a', 'e', 'i', 'o', 'u']
+vowels = ('aeiou')
 
-word = str.lower("Umuzi")
+word = str.lower(input())
 
-def my_function():
+def vowelsinword():
     list1 = list(word)
     list2 = []
     for x in list1:
         if x.lower() in vowels:
-            list2.append(x)
+            if x in vowels:
+             list2.append(x)
     vowes = ', '.join(list2)
-    print("Vowels: "+vowes)
-my_function()
+
+    print("Vowels: "+ vowes)
+vowelsinword()

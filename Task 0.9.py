4ceec5f3be2ad3195b852c12b734d108335611f8
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2021-10-22 11:19:46 +0200

    added my funciton

--- Task 0.9.py
+++ Task 0.9.py
@@ -2,10 +2,12 @@ vowels = ['a', 'e', 'i', 'o', 'u']
 
 word = str.lower("Umuzi")
 
-list1 = list(word)
-list2 = []
-for x in list1:
-    if x.lower() in vowels:
-        list2.append(x)
-vowes = ', '.join(list2)
-print("Vowels: "+vowes)
+def my_function():
+    list1 = list(word)
+    list2 = []
+    for x in list1:
+        if x.lower() in vowels:
+            list2.append(x)
+    vowes = ', '.join(list2)
+    print("Vowels: "+vowes)
+my_function()

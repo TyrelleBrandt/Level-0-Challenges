1a714ffab04e91372f0e5f758eaf772e3cc02c45
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2021-10-22 16:17:24 +0200

    changed function name

--- Task 0.5.py
+++ Task 0.5.py
@@ -1,9 +1,9 @@
-a = 4                   # a is the 1st side of a triangle.
-b = 6                   # b is the 2nd side of a triangle.
-c = 8                   # c is the 3rd side of a triangle.
-s = (a + b + c) / 2
+a = 4
+b = 6                
+c = 8
+s = int(a + b + c) / 2
 
-def my_function():
+def areatriangle_function():
     return (s * (s-a) * (s-b)* (s-c)) ** 0.5
 
-print(my_function())
+print(areatriangle_function())

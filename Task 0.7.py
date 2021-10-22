2e169376cb1c98162ac0235b24bbd609d1383abe
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2021-10-22 15:46:31 +0200

    changed function name

--- Task 0.7.py
+++ Task 0.7.py
@@ -1,12 +1,12 @@
 temp1 = 85
 temp2 = 25
 
-def my_function(temp1):
+def fahrenheit_to_celsius(temp1):
     celsius = (temp1 - 32) * 5/9
-    print(temp1)
-my_function(temp1)
+    print(celsius)
+fahrenheit_to_celsius(temp1)
 
-def my_function2(temp2):
+def celsius_to_fahrenheit(temp2):
     fahrenheit = (temp2 * 9/5) + 32
-    print(temp2)
-my_function2(temp2)
+    print(fahrenheit)
+celsius_to_fahrenheit(temp2)

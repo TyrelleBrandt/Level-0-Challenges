c2000968e4c68ee5eb465de559442e8f99b7ec62
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2021-10-22 11:29:39 +0200

    Update Task 0.10.py

--- Task 0.10.py
+++ Task 0.10.py
@@ -1,13 +1,15 @@
 str1 = "house"
 str2 = "computers"
 
-result = []
-if (len(str1) < len(str2)):
-    for a in str1:
-        if(a in str2):
-            result.append((a)+ ',')
-else:
-    for a in str2:
-        if(a in str1):
-            result.append((a)+ ', ')
-print('Common letters:', *result)
+def my_function():
+    result = []
+    if (len(str1) < len(str2)):
+        for a in str1:
+            if(a in str2):
+                result.append((a)+ ',')
+    else:
+        for a in str2:
+            if(a in str1):
+                result.append(a)
+    print('Common letters:', *result)
+my_function()

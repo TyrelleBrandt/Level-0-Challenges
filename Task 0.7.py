c7785da65f48713c49fb0722afd5b12013451285
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2021-10-22 11:11:42 +0200

    Update Task 0.7.py

--- Task 0.7.py
+++ Task 0.7.py
@@ -1,8 +1,12 @@
 temp1 = 85
 temp2 = 25
 
-celsius = (temp1 - 32) * 5/9
-print(temp1)
+def my_function(temp1):
+    celsius = (temp1 - 32) * 5/9
+    print(temp1)
+my_function(temp1)
 
-fahrenheit = (temp2 * 9/5) + 32
-print(temp2)
+def my_function2(temp2):
+    fahrenheit = (temp2 * 9/5) + 32
+    print(temp2)
+my_function2(temp2)

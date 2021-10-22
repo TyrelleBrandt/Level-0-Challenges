360f79a64af0626b0bb5cf8e3e5484146dc8c74b
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2021-10-22 16:29:54 +0200

    changed and added input function
    changed the function to "hours_and_minutes"
    added input function for any given number

--- Task 0.8.py
+++ Task 0.8.py
@@ -1,16 +1,16 @@
-number = 121
+number = int(input())
 a = str(1)
 
 hours = str(number // 60)
 minutes = str(number % 60)
 
-def my_function():
+def hours_and_minutes():
     if hours == a and minutes == a:
-        print(hours, "hour" + ',' + minutes, "minute")
+        print(hours, "hour" + ', ' + minutes, "minute")
     elif hours > a and minutes > a:
-        print(hours, "hours" + ',' + minutes, "minutes")
+        print(hours, "hours" + ', ' + minutes, "minutes")
     elif hours == a and minutes > a:
-        print(hours, "hour" + ',' + minutes, "minutes")
+        print(hours, "hour" + ', ' + minutes, "minutes")
     elif hours > a and minutes == a:
-        print(hours, "hours" + ',' + minutes, "minute")
-my_function()
+        print(hours, "hours" + ', ' + minutes, "minute")
+hours_and_minutes()

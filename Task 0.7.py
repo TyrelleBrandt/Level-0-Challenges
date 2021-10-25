4b97bf5a9e67dc341a55edcd77cd9690f205ade2
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2021-10-25 11:06:19 +0200

    Added Return

--- Task 0.7.py
+++ Task 0.7.py
@@ -1,12 +1,9 @@
-temp1 = 85
-temp2 = 25
-
 def fahrenheit_to_celsius(temp1):
     celsius = (temp1 - 32) * 5/9
-    print(celsius)
-fahrenheit_to_celsius(temp1)
+    return celsius
+print(fahrenheit_to_celsius(85))
 
 def celsius_to_fahrenheit(temp2):
     fahrenheit = (temp2 * 9/5) + 32
-    print(fahrenheit)
-celsius_to_fahrenheit(temp2)
+    return fahrenheit
+print(celsius_to_fahrenheit(25))

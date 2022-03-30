38a7a0f8a2d636282bef127a1780972cf4f41364
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2022-03-30 14:26:14 +0200

    Update task_0_8.py

--- task_0_8.py
+++ task_0_8.py
@@ -1,14 +1,14 @@
 def hours_and_minutes(number):
-    a = str(1)
+    range = str(1)
     hours = str(number // 60)
     minutes = str(number % 60)
-    if hours == a and minutes == a:
+    if hours == range and minutes == range:
         print(hours, "hour" + ", " + minutes, "minute")
-    elif hours > a and minutes > a:
+    elif hours > range and minutes > range:
         print(hours, "hours" + ", " + minutes, "minutes")
-    elif hours == a and minutes > a:
+    elif hours == range and minutes > range:
         print(hours, "hour" + ", " + minutes, "minutes")
-    elif hours > a and minutes == a:
+    elif hours > range and minutes == range:
         print(hours, "hours" + ", " + minutes, "minute")
 
 

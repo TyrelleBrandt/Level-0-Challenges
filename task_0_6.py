971ee53c3a19a8cada5019ef40f9bc8252426693
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2022-03-30 14:22:28 +0200

    Update task_0_6.py

--- task_0_6.py
+++ task_0_6.py
@@ -1,11 +1,11 @@
-def maximum_number(a, b, c):
-    if (a >= b) and (a >= c):
-        largest = a
-    elif (b >= a) and (b >= c):
-        largest = b
-    elif (c >= a) and (c >= b):
-        largest = c
+def maximum_number_given(no_1, no_2, no_3):
+    if (no_1 >= no_2) and (no_1 >= no_3):
+        largest = no_1
+    elif (no_2 >= no_1) and (no_2 >= no_3):
+        largest = no_2
+    elif (no_3 >= no_1) and (no_3 >= no_2):
+        largest = no_3
     return largest
 
 
-print(maximum_number(10, 20, 25))
+print(maximum_number_given(10, 20, 25))

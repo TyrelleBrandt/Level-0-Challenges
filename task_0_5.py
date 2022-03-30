af8cbfe7857ca1a647472a5da7205b6759aaa322
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2022-03-30 14:19:30 +0200

    Update task_0_5.py

--- task_0_5.py
+++ task_0_5.py
@@ -1,6 +1,6 @@
-def area_triangle_function(a, b, c):
-    s = int(a + b + c) / 2
-    return (s * (s - a) * (s - b) * (s - c)) ** 0.5
+def area_of_a_triangle(side_a, side_b, side_c):
+    semi_perimeter = int(side_a + side_b + side_c) / 2
+    return (semi_perimeter * (semi_perimeter - side_a) * (semi_perimeter - side_b) * (semi_perimeter - side_c)) ** 0.5
 
 
-print(area_triangle_function(4, 6, 8))
+print(area_of_a_triangle(4, 6, 8))

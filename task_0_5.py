754376df54dcdc2615620022c1e105de31dcdf4e
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2022-04-06 10:34:18 +0200

    Update task_0_5.py

--- task_0_5.py
+++ task_0_5.py
@@ -1,5 +1,5 @@
 def area_of_a_triangle(side_a, side_b, side_c):
-    semi_perimeter = int(side_a + side_b + side_c) / 2
+    semi_perimeter = (side_a + side_b + side_c) / 2
     return (semi_perimeter * (semi_perimeter - side_a) * (semi_perimeter - side_b) * (semi_perimeter - side_c)) ** 0.5
 
 

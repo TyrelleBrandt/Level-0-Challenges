47ddfa926950d9ba5e4b658173f422adb3e2cad8
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2022-04-13 15:51:24 +0200

    Update task_0_8.py

--- task_0_8.py
+++ task_0_8.py
@@ -6,10 +6,12 @@ def hours_and_minutes(number):
         print(f"{hours} hours, {minutes} minutes")
     elif hours > range and minutes > range:
         print(f"{hours} hours, {minutes} minutes")
-    elif hours == range and minutes > range:
+    elif hours == str(1) and minutes > range:
         print(f"{hours} hour, {minutes} minutes")
-    elif hours > range and minutes == range:
+    elif hours > range and minutes == str(1):
         print(f"{hours} hours, {minutes} minute")
+    elif hours == str(1) and minutes == str(1):
+        print(f"{hours} hour, {minutes} minute")
 
 
 hours_and_minutes(700)

ce4c805f19d021d0d8ea0b68a7bd322797bf6fab
TyrelleBrandt <91653774+TyrelleBrandt@users.noreply.github.com>
2022-04-14 10:27:54 +0200

    Update task_0_9.py

--- task_0_9.py
+++ task_0_9.py
@@ -3,10 +3,9 @@ def vowels_in_a_word(word):
     list1 = word.lower()
     list2 = []
     for x in list1:
-        if x.lower() in vowels:
             if x in vowels:
                 list2.append(x)
-    vowes = list(dict.fromkeys(list2))
+    vowes = list(set(list2))
     final_vowels = ", ".join(vowes)
     return "Vowels: " + final_vowels
 
